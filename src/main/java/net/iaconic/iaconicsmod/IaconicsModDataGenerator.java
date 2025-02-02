805368aa5c4be7a749d873caf0e6cf38e21e30b5
Iaconic <rehoboth01@outlook.com>
2025-02-02 21:57:51 +0000

    initialCommit

---
+++ src/main/java/net/iaconic/iaconicsmod/IaconicsModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.iaconic.iaconicsmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class IaconicsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
