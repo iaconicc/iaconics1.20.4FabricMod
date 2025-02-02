4b248865ab96279faa551156fc49ba408200b00c
Iaconic <rehoboth01@outlook.com>
2025-02-02 23:00:51 +0000

    client side only code added

---
+++ src/main/java/net/iaconic/iaconicsmod/IaconisModClient.java
@@ -0,0 +1,10 @@
+package net.iaconic.iaconicsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IaconisModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
