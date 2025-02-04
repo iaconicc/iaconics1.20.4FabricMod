1793a2aaa8d98370c06a2433b66da4d440373b54
Iaconic <rehoboth01@outlook.com>
2025-02-04 11:17:53 +0000

    custom groups

--- src/main/java/net/iaconic/iaconicsmod/blocks/Blocks.java
+++ src/main/java/net/iaconic/iaconicsmod/blocks/Blocks.java
@@ -1,15 +1,11 @@
 package net.iaconic.iaconicsmod.blocks;
 
-import net.iaconic.iaconicsmod.items.CustomItemGroups;
+
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.util.Identifier;
 
 public class Blocks {
     public static BaseBlock newBlock;
